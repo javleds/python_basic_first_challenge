a24a474832b1cc5c0758a3ded1750ef43ff96a77
Javier <juls0593@gmail.com>
2021-05-18 15:34:51 -0500

    Add test for get_area

--- tests/test_triangle_utis.py
+++ tests/test_triangle_utis.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from nose2.tools import params
 from src.triangle import Triangle
-from src.triangle_utils import get_type
+from src.triangle_utils import get_type, get_area
 
 
 class TestTriangleUtils(TestCase):
@@ -12,3 +12,10 @@ class TestTriangleUtils(TestCase):
     )
     def test_get_type(self, triangle, expected):
         self.assertEqual(get_type(triangle), expected)
+
+    @params(
+        (Triangle(10, 10, 10), 43.30127018922193)
+    )
+    def test_get_area(self, triangle, expected):
+        self.assertEqual(get_area(triangle), expected)
+
